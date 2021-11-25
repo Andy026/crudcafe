795d28403a99e26b1b7d1d168a9895e01de09527
Andy <andysatie@gmail.com>
2021-11-25 10:09:14 -0300

    se terminaron de agregar las validaciones

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,28 +1,12 @@
-let nombre = document.querySelector('#nombre');
-let precio = document.querySelector('#precio');
-
-nombre.addEventListener('blur', () => {validarCampoRequerido(nombre)});
-precio.addEventListener('blur', () => {validarNumero(precio)});
-
-function validarCampoRequerido(input) {
-  if (input.value.trim().length > 0 && input.value.trim().length >= 3) {
-    input.className = "form-control rounded-pill border-1 is-valid";
-    console.log(input.value)
+export const campoRequerido = (valor) => {
+  if (valor.trim().length > 0) {
     return true;
   } else {
-    input.className = "form-control rounded-pill border-1 is-invalid";
     return false;
   }
-}
+};
 
-function validarNumero(input) {
-    let patron = /^[0-9]{1,5}$/;
-    if (patron.test(input.value)) {
-      input.className = "form-control rounded-pill border-1 is-valid";
-      console.log(input.value)
-      return true;
-    } else {
-      input.className = "form-control rounded-pill border-1 is-invalid";
-      return false;
-    }
-  }
+export const rangoNumero = (valor) => {
+  if (valor > 0 && valor < 5000) return true;
+  else return false;
+};
--- src/components/productos/AgregarProducto.js
+++ src/components/productos/AgregarProducto.js
@@ -1,27 +1,45 @@
 import React, {useState} from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
+import { campoRequerido, rangoNumero } from "../helpers/helpers";
 
 const AgregarProducto = () => {
 
   const [nombreProducto, setNombreProducto] = useState('');
   const [precioProducto, setPrecioProducto] = useState(0);
   const [categoria, setCategoria] = useState('');
+  const [error, setError] = useState(false);
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //validar los datos del formm
+    if(campoRequerido(nombreProducto) &&
+    rangoNumero(precioProducto) &&
+    campoRequerido(categoria)){
+      //reset del state de error
+      setError(false);
+      //crear un producto y enviar a la API
+    }else{
+      //mostrar al usuario un cartel de error si algo malió sal
+      setError(true);
+    }
+  }
 
   return (
     <div className="container">
       <h1 className="text-center display-2 my-3">Agregar Producto</h1>
-      <Form className="border border-dark border-2 rounded py-3 px-3 my-5">
-        <Form.Group className="mb-3" controlId="">
+      <Form className="border border-dark border-2 rounded py-3 px-3 my-5" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
           <Form.Label>Nombre:</Form.Label>
           <Form.Control id="nombre" className="border border-dark" type="text" placeholder="" onChange={event => setNombreProducto(event.target.value)}/>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="">
+        <Form.Group className="mb-3">
           <Form.Label>Precio:</Form.Label>
           <Form.Control id="precio" className="border border-dark" type="text" placeholder="" onChange={event => setPrecioProducto(event.target.value)}/>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="">
+        <Form.Group className="mb-3">
           <Form.Label>Categoria:</Form.Label>
-            <Form.Select className="border border-dark" onChange={event => setCategoria(event.target)}>
+            <Form.Select className="border border-dark" onChange={event => setCategoria(event.target.value)}>
               <option value=''>Seleccione una opción</option>
               <option value='bebida-caliente'>Bebida Caliente</option>
               <option value='bebida-fria'>Bebida Fria</option>
@@ -34,6 +52,11 @@ const AgregarProducto = () => {
           Guardar
         </Button>
       </Form>
+      { error === true ?
+      <Alert variant='danger'>
+    Debe cargar todos los datos y el precio debe estar entre 1 y $4999
+  </Alert>
+  : null}
     </div>
   );
 };
